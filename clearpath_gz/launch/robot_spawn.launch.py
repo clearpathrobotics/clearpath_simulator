8c0a8872b665a705a38aa51cc52ff74eeecbbbd7
luis-camero <88782189+luis-camero@users.noreply.github.com>
2025-03-06 10:07:28 -0500

    Add argument to disable generation (#74)

--- clearpath_gz/launch/robot_spawn.launch.py
+++ clearpath_gz/launch/robot_spawn.launch.py
@@ -50,7 +50,11 @@ ARGUMENTS = [
                           description='Gazebo World'),
     DeclareLaunchArgument('setup_path',
                           default_value=[EnvironmentVariable('HOME'), '/clearpath/'],
-                          description='Clearpath setup path')
+                          description='Clearpath setup path'),
+    DeclareLaunchArgument('generate',
+                          default_value='true',
+                          choices=['true', 'false'],
+                          description='Generate parameters and launch files')
 ]
 
 for pose_element in ['x', 'y', 'yaw']:
@@ -67,6 +71,7 @@ def launch_setup(context, *args, **kwargs):
     use_sim_time = LaunchConfiguration('use_sim_time')
     x, y, z = LaunchConfiguration('x'), LaunchConfiguration('y'), LaunchConfiguration('z')
     yaw = LaunchConfiguration('yaw')
+    generate = LaunchConfiguration('generate')
 
     # Parse robot YAML into config
     clearpath_config = ClearpathConfig(os.path.join(
@@ -123,6 +128,7 @@ def launch_setup(context, *args, **kwargs):
         executable='generate_description',
         name='generate_description',
         output='screen',
+        condition=IfCondition(generate),
         arguments=['-s', setup_path]
     )
 
@@ -131,6 +137,7 @@ def launch_setup(context, *args, **kwargs):
         executable='generate_semantic_description',
         name='generate_semantic_description',
         output='screen',
+        condition=IfCondition(generate),
         arguments=['-s', setup_path]
     )
 
@@ -139,6 +146,7 @@ def launch_setup(context, *args, **kwargs):
         executable='generate_launch',
         name='generate_launch',
         output='screen',
+        condition=IfCondition(generate),
         arguments=['-s', setup_path]
     )
 
@@ -147,6 +155,7 @@ def launch_setup(context, *args, **kwargs):
         executable='generate_param',
         name='generate_launch',
         output='screen',
+        condition=IfCondition(generate),
         arguments=['-s', setup_path]
     )
 
@@ -187,7 +196,7 @@ def launch_setup(context, *args, **kwargs):
         condition=IfCondition(LaunchConfiguration('rviz')),
     )
 
-    return [
+    actions = [
         node_generate_description,
         event_generate_description,
         event_generate_semantic_description,
@@ -195,6 +204,11 @@ def launch_setup(context, *args, **kwargs):
         event_generate_param,
         rviz
     ]
+
+    if generate.perform(context):
+        actions.append(group_action_spawn_robot)
+
+    return actions
 
 
 def generate_launch_description():
