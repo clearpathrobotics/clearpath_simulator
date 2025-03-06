c0a4fa0d50b29dc28cf0265950ef63d708142e9b
Luis Camero <lcamero@clearpathrobotics.com>
2025-03-06 10:31:21 -0500

    Fix

--- clearpath_gz/launch/robot_spawn.launch.py
+++ clearpath_gz/launch/robot_spawn.launch.py
@@ -205,7 +205,7 @@ def launch_setup(context, *args, **kwargs):
         rviz
     ]
 
-    if generate.perform(context):
+    if not generate.perform(context):
         actions.append(group_action_spawn_robot)
 
     return actions
