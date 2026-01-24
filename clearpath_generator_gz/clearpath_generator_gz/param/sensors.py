c0e1f04213b1a09b81e8f1f040333c103bf64ebf
Andrei Costinescu <AndreiCostinescu@users.noreply.github.com>
2026-01-24 07:54:18 +0100

    Reorganize imports for 3D lidars in sensors.py
    Fix imports out-of-alphabetical-order and line too long

--- clearpath_generator_gz/clearpath_generator_gz/param/sensors.py
+++ clearpath_generator_gz/clearpath_generator_gz/param/sensors.py
@@ -48,7 +48,12 @@ from clearpath_config.sensors.types.gps import (
 )
 from clearpath_config.sensors.types.imu import BaseIMU, CHRoboticsUM6, Microstrain, RedshiftUM7
 from clearpath_config.sensors.types.lidars_2d import BaseLidar2D, HokuyoUST, SickLMS1XX
-from clearpath_config.sensors.types.lidars_3d import BaseLidar3D, OusterOS1, VelodyneLidar, SeyondLidar
+from clearpath_config.sensors.types.lidars_3d import (
+    BaseLidar3D,
+    OusterOS1,
+    SeyondLidar,
+    VelodyneLidar,
+)
 from clearpath_config.sensors.types.sensor import BaseSensor
 
 
